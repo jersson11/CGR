7c23799788789b84655dcd0482def924da1345fc
Nicolas Sosa <Nicolas.sosa@tecsersas.com>
2024-11-28 10:52:27 -0500

    development: generic mapper added

--- src/main/java/com/test/testactivedirectory/application/role/service/RoleServiceImpl.java
+++ src/main/java/com/test/testactivedirectory/application/role/service/RoleServiceImpl.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.test.testactivedirectory.application.role.dto.RoleRequestDto;
 import com.test.testactivedirectory.application.role.usecase.RoleService;
 import com.test.testactivedirectory.domain.repository.IRoleRepository;
 import com.test.testactivedirectory.infrastructure.persistence.entity.RoleEntity;
+import com.test.testactivedirectory.infrastructure.utilities.DtoMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -17,34 +19,36 @@ public class RoleServiceImpl implements RoleService {
 
     private final IRoleRepository roleRepository;
 
-    @Transactional(readOnly = true)
-    @Override
-    public List<RoleEntity> findAll() {
-        return this.roleRepository.findAll();
-    }
+    private final DtoMapper dtoMapper;
 
     @Transactional(readOnly = true)
     @Override
-    public RoleEntity findById(Long idRole) {
-        return this.roleRepository.findById(idRole);
+    public List<RoleRequestDto> findAll() {
+        return this.dtoMapper.convertToListDto(this.roleRepository.findAll(), RoleRequestDto.class);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public RoleRequestDto findById(Long idRole) {
+        return this.dtoMapper.convertToDto(this.roleRepository.findById(idRole), RoleRequestDto.class);
     }
 
     @Transactional
     @Override
-    public RoleEntity create(RoleEntity roleEntity) {
-        return this.roleRepository.create(roleEntity);
+    public RoleRequestDto create(RoleEntity roleEntity) {
+        return this.dtoMapper.convertToDto(this.roleRepository.create(roleEntity), RoleRequestDto.class);
     }
 
     @Transactional
     @Override
-    public RoleEntity update(RoleEntity roleEntity) {
-        return this.roleRepository.update(roleEntity);
+    public RoleRequestDto update(RoleEntity roleEntity) {
+        return this.dtoMapper.convertToDto(this.roleRepository.update(roleEntity), RoleRequestDto.class);
     }
 
     @Transactional
     @Override
-    public RoleEntity activateOrDeactivate(Long idRole) {
-        return this.roleRepository.activateOrDeactivate(idRole);
+    public RoleRequestDto activateOrDeactivate(Long idRole) {
+        return this.dtoMapper.convertToDto(this.roleRepository.activateOrDeactivate(idRole), RoleRequestDto.class);
     }
 
 }
--- src/main/java/com/test/testactivedirectory/application/role/usecase/RoleService.java
+++ src/main/java/com/test/testactivedirectory/application/role/usecase/RoleService.java
@@ -2,16 +2,17 @@ package com.test.testactivedirectory.application.role.usecase;
 
 import java.util.List;
 
+import com.test.testactivedirectory.application.role.dto.RoleRequestDto;
 import com.test.testactivedirectory.infrastructure.persistence.entity.RoleEntity;
 
 public interface RoleService {
-    public abstract List<RoleEntity> findAll();
+    public abstract List<RoleRequestDto> findAll();
 
-    public abstract RoleEntity findById(Long idRole);
+    public abstract RoleRequestDto findById(Long idRole);
 
-    public abstract RoleEntity create(RoleEntity roleEntity);
+    public abstract RoleRequestDto create(RoleEntity roleEntity);
 
-    public abstract RoleEntity update(RoleEntity roleEntity);
+    public abstract RoleRequestDto update(RoleEntity roleEntity);
 
-    public abstract RoleEntity activateOrDeactivate(Long idRole);
+    public abstract RoleRequestDto activateOrDeactivate(Long idRole);
 }
